aedf7165375358ed79c6072473e3f93c66c7b82a
ringe <ringe@STEVES-PC>
2016-12-20 22:57:27 -0700

    Add project files.

---
+++ ForEachDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEachDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new List<string> { "Steve", "Joe", "Mary", "Julie" }
+            .ForEach(name => Console.WriteLine(name));
+            Console.ReadLine();
+
+            new List<string> { "Tom", "Dick", "Harry" }
+            .ForEach(n => Console.WriteLine(n));
+            Console.ReadLine();
+        }
+    }
+}
